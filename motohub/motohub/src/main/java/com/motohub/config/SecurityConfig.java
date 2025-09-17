bcf2e189c130ccad2dfb8ba693155a6801b3a68e
SergioMesa2002 <139014710+SergioMesa2002@users.noreply.github.com>
2025-09-17 15:07:08 -0500

    rutas

--- motohub/motohub/src/main/java/com/motohub/config/SecurityConfig.java
+++ motohub/motohub/src/main/java/com/motohub/config/SecurityConfig.java
@@ -4,17 +4,47 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
 @Configuration
 public class SecurityConfig {
 
     @Bean
-
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> {}) // 👉 habilitamos CORS
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()  // 👉 Todas las rutas públicas
+                        .anyRequest().permitAll() // todas las rutas públicas
                 );
         return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+
+        // 👉 habilitamos Live Server
+        config.setAllowedOrigins(Arrays.asList(
+                "http://127.0.0.1:5503",
+                "http://localhost:5503"
+        ));
+
+        config.setAllowedHeaders(Arrays.asList(
+                "Origin", "Content-Type", "Accept", "Authorization"
+        ));
+        config.setAllowedMethods(Arrays.asList(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+        ));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
