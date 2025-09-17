fa39be45e3fbe8b548683d910bd25ea51b33fafe
SergioMesa2002 <139014710+SergioMesa2002@users.noreply.github.com>
2025-09-17 16:29:37 -0500

    arreglo

---
+++ motohub/motohub/src/main/java/com/motohub/dto/DtoUser.java
@@ -0,0 +1,23 @@
+package com.motohub.dto;
+
+public class DtoUser {
+
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
